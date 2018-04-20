633b3b374da462c04208344c500ae7272e787382
Pentagon <pentagonalpentagon@gmail.com>
2018-04-20 17:35:44 +0800

    last abnormality update I swear holy heck

--- index.js
+++ index.js
@@ -804,7 +804,7 @@ module.exports = function FpsUtils2(dispatch) {
         if (config.blacklistAbnormies && config.hiddenAbnormies.includes(event.id)) {
             return false;
         }
-        if (config.hideAllAbnormies && !event.target.equals(myId) && (spawnedPlayers[event.target] && event.source)) {
+        if (config.hideAllAbnormies && !event.target.equals(myId) && (spawnedPlayers[event.target] && spawnedPlayers[event.source])) {
             return false;
         }
     });
